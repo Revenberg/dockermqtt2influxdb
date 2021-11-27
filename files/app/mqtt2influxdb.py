21387b032fe1acea08b67f4d99024c9301fc3e33
Sander Revenberg <sander@revenberg.info>
2021-11-27 12:12:25 +0100

    .

--- files/app/mqtt2influxdb.py
+++ files/app/mqtt2influxdb.py
@@ -202,13 +202,13 @@ def _init_influxdb_database():
     influxdb_client.create_retention_policy('infinite', 'INF', 1, INFLUXDB_DATABASE, default=False)
     
     influxdb_client.drop_continuous_query(INFLUXDB_TABEL + "_30_days", INFLUXDB_DATABASE)
-    select_clause = 'SELECT mean(*) INTO "30_days"."' + INFLUXDB_TABEL + '" FROM "10_days"' + INFLUXDB_TABEL + '" GROUP BY time(5m)'
+    select_clause = 'SELECT mean(*) INTO "30_days"."' + INFLUXDB_TABEL + '" FROM "10_days"."' + INFLUXDB_TABEL + '" GROUP BY time(5m)'
     logging.debug(select_clause)
-    
+
     influxdb_client.create_continuous_query(INFLUXDB_TABEL + "_30_days", INFLUXDB_DATABASE, select_clause, INFLUXDB_DATABASE, 'EVERY 10s FOR 5m')
 
     influxdb_client.drop_continuous_query(INFLUXDB_TABEL + "_infinite", INFLUXDB_DATABASE)
-    select_clause = 'SELECT mean(*) INTO "infinite"."' + INFLUXDB_TABEL + '" FROM "10_days"' + INFLUXDB_TABEL + '" GROUP BY time(5m)'
+    select_clause = 'SELECT mean(*) INTO "infinite"."' + INFLUXDB_TABEL + '" FROM "10_days"."' + INFLUXDB_TABEL + '" GROUP BY time(5m)'
     influxdb_client.create_continuous_query(INFLUXDB_TABEL + '_infinite', select_clause, INFLUXDB_DATABASE, 'EVERY 360s FOR 60m')
 
     influxdb_client.switch_database(INFLUXDB_DATABASE)
